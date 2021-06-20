5096386286ae7ff29bbf8088d26df99df1890818
Sumith Ekanayake <sumithe@embla.asia>
2021-06-20 20:15:37 +0530

    Initial commit backend and client

---
+++ socket-backend/src/chat.gateway.ts
@@ -0,0 +1,12 @@
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway()
+export class ChatGateway {
+    @WebSocketServer()
+    server;
+
+    @SubscribeMessage('message')
+    handleMessage(@MessageBody() message: string): void {
+        this.server.emit('message', message);
+    }
+}
